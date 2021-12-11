be98e2d68d23dce1ef14b7a1303d424228527706
huntedhappy <huntedhappy@gmail.com>
2021-12-11 19:51:22 +0900

    java-flow-control

---
+++ java-flow-control/AuthApp3.java
@@ -0,0 +1,34 @@
+
+public class AuthApp3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//String[] users = {"egoing", "jinhuck", "youbin"};
+		
+		String[][] users = {
+				{"egoing","1111"},
+				{"jinhyuck","2222"},
+				{"youbin","1111"}
+		};
+		String inputId = args[0];
+		String inputPass = args[1];
+		
+		boolean isLogined = false;
+		for (int i=0; i < users.length; i++) {
+			String[] current = users[i];
+			if(current[0].equals(inputId) && current[1].equals(inputPass)) {
+				isLogined = true;
+			    break;
+			}
+			
+		}
+		System.out.println("Hi,");
+		if(isLogined) {
+			System.out.println("Master!!");
+		} else {
+			System.out.println("Who are you?");
+		}
+
+	}
+
+}
